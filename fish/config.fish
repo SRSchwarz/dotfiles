57d2778c0881f9093d5e45e5806884a4aa5825b4
Simon Schwarz <simon.schwarz884@gmail.com>
2024-02-17 11:28:33 +0100

    Update ta shortcut

--- fish/config.fish
+++ fish/config.fish
@@ -6,7 +6,7 @@ set fish_greeting
 abbr cf nvim ~/.config/fish/config.fish
 abbr cn nvim ~/.config/nvim
 abbr ck nvim ~/.config/kitty/kitty.conf
-abbr ta tmux attach -t
+abbr ta "tmux new -As (pwd)"
 abbr ls eza --all --icons
 abbr gs git status
 abbr gca "git add . && git commit -m"
